0e7864f880365d0a7f10e4fd1b9daff9672f6918
Aaron Molina <2aaronmolina@gmail.com>
2020-04-22 11:58:54 -0400

    Updates

---
+++ precache-manifest.70f98500f8b39440a01b64989a6053b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d6ae54fa64c341bd698ed5c11525bed",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "9df279e103bc0bd4c251",
+    "url": "/react-todo-app/static/css/main.e6d81a11.chunk.css"
+  },
+  {
+    "revision": "94c9457dd9ff0e91acf6",
+    "url": "/react-todo-app/static/js/2.a7f94886.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todo-app/static/js/2.a7f94886.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9df279e103bc0bd4c251",
+    "url": "/react-todo-app/static/js/main.7ee5447a.chunk.js"
+  },
+  {
+    "revision": "1796198026348bea5667",
+    "url": "/react-todo-app/static/js/runtime-main.3af68235.js"
+  }
+]);
